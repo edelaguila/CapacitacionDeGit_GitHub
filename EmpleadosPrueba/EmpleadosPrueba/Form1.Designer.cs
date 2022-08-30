35a91e36517e6bf43d699a786fa90292ad3ec0dc
unknown <jmmarroquinr00@gmail.com>
2022-08-30 13:17:36 -0600

    odbc

---
+++ EmpleadosPrueba/EmpleadosPrueba/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace EmpleadosPrueba
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Txt_nombre = new System.Windows.Forms.TextBox();
+            this.Txt_depto = new System.Windows.Forms.TextBox();
+            this.Txt_puesto = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 101);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 148);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Puesto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 197);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Departamento";
+            // 
+            // Txt_nombre
+            // 
+            this.Txt_nombre.Location = new System.Drawing.Point(161, 101);
+            this.Txt_nombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_nombre.Name = "Txt_nombre";
+            this.Txt_nombre.Size = new System.Drawing.Size(291, 22);
+            this.Txt_nombre.TabIndex = 4;
+            // 
+            // Txt_depto
+            // 
+            this.Txt_depto.Location = new System.Drawing.Point(161, 197);
+            this.Txt_depto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_depto.Name = "Txt_depto";
+            this.Txt_depto.Size = new System.Drawing.Size(291, 22);
+            this.Txt_depto.TabIndex = 5;
+            // 
+            // Txt_puesto
+            // 
+            this.Txt_puesto.Location = new System.Drawing.Point(161, 148);
+            this.Txt_puesto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_puesto.Name = "Txt_puesto";
+            this.Txt_puesto.Size = new System.Drawing.Size(291, 22);
+            this.Txt_puesto.TabIndex = 6;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(173, 303);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(100, 28);
+            this.btnAgregar.TabIndex = 7;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(281, 303);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 386);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.Txt_puesto);
+            this.Controls.Add(this.Txt_depto);
+            this.Controls.Add(this.Txt_nombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Txt_nombre;
+        private System.Windows.Forms.TextBox Txt_depto;
+        private System.Windows.Forms.TextBox Txt_puesto;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ EmpleadosPrueba/EmpleadosPrueba/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.Odbc;
+
+namespace EmpleadosPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Conexion cn = new Conexion();
+
+        
+
+        void insertarEmpleado()
+        {
+            string nombre, puesto, departamento;
+            nombre = Txt_nombre.Text;
+            puesto = Txt_puesto.Text;
+            departamento = Txt_depto.Text;
+
+            
+
+            string cadena = "INSERT INTO empleados (nombre_completo, puesto, departamento) VALUES('"+nombre+"','"+puesto+"','"+departamento+"');" ;
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            insertarEmpleado();
+            Txt_nombre.Text = "";
+            Txt_depto.Text = "";
+            Txt_puesto.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Consulta b = new Consulta();
+            b.Show();
+            this.Hide();
+        }
+    }
+}
