e617e8beb4c447916bfd0676907d8fac40986bef
YordiHernandez <superyo234@gmail.com>
2022-09-01 07:51:13 -0600

    Prueba ODBC

---
+++ PracticaODBC/EmpleadosPrueba/EmpleadosPrueba/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+
+namespace EmpleadosPrueba
+{
+    public partial class consulta : Form
+    {
+        public consulta()
+        {
+            InitializeComponent();
+        }
+        conexion cn = new conexion();
+
+        void cargar()
+        {
+
+            string cadena = "SELECT * FROM empleados";
+
+            OdbcDataAdapter datos = new OdbcDataAdapter(cadena, cn.conectar());
+
+            DataTable dt = new DataTable();
+
+            datos.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+
+
+        private void consulta_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cargar();
+        }
+    }
+}
