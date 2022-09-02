18eec37f1b15e987f487ea7c17c2c7e4b47e9c96
Amaya94 <jamayao@miumg.edu.gt>
2022-09-01 19:34:35 -0600

    Practica Empleados OBDC

---
+++ PracticaODBC/PracticaOBDC Josue Amaya 0901-19-12421/EmpleadosPrueba/Consulta.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EmpleadosPrueba
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_consultar = new System.Windows.Forms.Button();
+            this.dgv_1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_consultar
+            // 
+            this.btn_consultar.Location = new System.Drawing.Point(509, 32);
+            this.btn_consultar.Name = "btn_consultar";
+            this.btn_consultar.Size = new System.Drawing.Size(131, 45);
+            this.btn_consultar.TabIndex = 0;
+            this.btn_consultar.Text = "Consultar";
+            this.btn_consultar.UseVisualStyleBackColor = true;
+            this.btn_consultar.Click += new System.EventHandler(this.btn_consultar_Click);
+            // 
+            // dgv_1
+            // 
+            this.dgv_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_1.Location = new System.Drawing.Point(27, 129);
+            this.dgv_1.Name = "dgv_1";
+            this.dgv_1.Size = new System.Drawing.Size(613, 263);
+            this.dgv_1.TabIndex = 1;
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 450);
+            this.Controls.Add(this.dgv_1);
+            this.Controls.Add(this.btn_consultar);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            this.Load += new System.EventHandler(this.Consulta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_consultar;
+        private System.Windows.Forms.DataGridView dgv_1;
+    }
+}
---
+++ PracticaODBC/PracticaOBDC Josue Amaya 0901-19-12421/EmpleadosPrueba/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.Odbc;
+using System.Windows.Forms;
+
+namespace EmpleadosPrueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        Conexion cn = new Conexion();
+
+        void insertarempleado()
+        {
+            String cadena = "INSERT INTO empleados (nombre_completo,puesto,departamento) VALUES (" + name.Text + "," + puesto.Text + ", " + depto.Text + ");";
+            OdbcCommand consulta = new OdbcCommand(cadena, cn.conexion());
+            consulta.ExecuteNonQuery();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            insertarempleado();
+            name.Text = "";
+            puesto.Text = "";
+            depto.Text = "";
+        }
+
+        private void btnDatos_Click(object sender, EventArgs e)
+        {
+            Consulta cs = new Consulta();
+            cs.Show();
+            this.Hide();
+        }
+    }
+}
