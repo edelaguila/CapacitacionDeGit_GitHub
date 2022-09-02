2265bcac3353f87f283a47b703a27ae19d296370
AlejandroCol8 <kevin_gozalez16utlook.es>
2022-09-01 21:54:08 -0600

    Practica ODBC

---
+++ PracticaODBC/PracticaODBC/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaODBC
+{
+    public partial class Form2 : Form
+    {
+        Conexion cn = new Conexion();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        void cargar()
+        {
+            string cadena = "SELECT * FROM empleados";
+            OdbcDataAdapter datos = new OdbcDataAdapter(cadena, cn.conexion());
+
+            DataTable dt = new DataTable();
+
+            datos.Fill(dt);
+
+            Dgb_1.DataSource = dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cargar();
+        }
+    }
+}
