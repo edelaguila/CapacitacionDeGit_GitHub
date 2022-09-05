accb260758b174c3bfd3c196aa0143a06cf13ca2
Joshua53Barrios <jbarrioso3@miumg.edu.gt>
2022-09-04 21:15:26 -0600

    Subiendo Practica MVC

---
+++ PracticaMVC/MVC/PracticaMVC/Capa_Vista/Consulta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Capa_Controlador;
+
+namespace Capa_Vista
+{
+    public partial class Consulta : Form
+    {
+        string emp = "empleados";
+        Controlador cn = new Controlador();
+
+
+
+
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTbl(emp);
+            Dtg1.DataSource = dt;
+
+        }
+
+
+        private void Consulta_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCons_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+    }
+}
