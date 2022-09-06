2ffaba3068ef1a15116a27aeea40fe45c245ed8e
Luis Torres <ltorresb@miumg.edu.gt>
2022-09-06 11:58:48 -0600

    Capacitacion MVC

---
+++ Practica MVC/PruebaMVC/Controlador/CControlador.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using Modelo;
+
+namespace Controlador
+{
+    public class CControlador
+    {
+        Sentencias sn = new Sentencias();
+
+        public DataTable llenartbl(string tabla)
+        {
+            OdbcDataAdapter dt = sn.llenartbl(tabla);
+            DataTable table = new DataTable();
+            dt.Fill(table);
+            return table;
+        }
+        
+
+
+    }
+}
