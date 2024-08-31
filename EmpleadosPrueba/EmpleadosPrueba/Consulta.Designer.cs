edf9dbe54476766a43bf890ae6acb015ef78802b
JdanielS7 <jsierrac3@miumg.edu.gt>
2024-08-31 12:25:02 -0600

    reorganización de archivos de capacitaciones

---
+++ EmpleadosPrueba/EmpleadosPrueba/Consulta.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EmpleadosPrueba
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Dgv_1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 94);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Consulta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Dgv_1
+            // 
+            this.Dgv_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_1.Location = new System.Drawing.Point(66, 165);
+            this.Dgv_1.Name = "Dgv_1";
+            this.Dgv_1.Size = new System.Drawing.Size(658, 218);
+            this.Dgv_1.TabIndex = 1;
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Dgv_1);
+            this.Controls.Add(this.button1);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView Dgv_1;
+    }
+}
